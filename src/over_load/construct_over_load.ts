76a6614eb588222049344c84cc06f6bf549576d2
ykw <1794093413@qq.com>
2022-04-13 21:44:06 +0800

    feat: 完善构造函数重载

--- src/over_load/construct_over_load.ts
+++ src/over_load/construct_over_load.ts
@@ -1,12 +1,23 @@
 type type_ChartParam = {
-    width?:number,
+    width?: number,
     height: number,
     radius?: number
 }
 class Square {
     public width!: number;
     public height!: number;
-    public getArea():number {
-        return this.height*this.width
+    constructor(width_: number, height_: number)//重载签名
+    constructor(obj: type_ChartParam)//重载签名
+    constructor(objOrWidth_: any,height_:number = 0){
+        if(typeof objOrWidth_ === 'object'){
+
+        }else{
+            this.width = objOrWidth_
+            this.height = height_
+        }
+
+    }
+    public getArea(): number {
+        return this.height * this.width
     }
 }

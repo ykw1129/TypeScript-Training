071c731c1fc22aec1ecd4ba2b3b3e8535576a85c
ykw <1794093413@qq.com>
2022-04-18 22:24:59 +0800

    feat: 完成饿汉式和懒汉式的书写

--- src/Singleton Pattern/Singleton_Pattern[饿汉式].ts
+++ src/Singleton Pattern/Singleton_Pattern[饿汉式].ts
@@ -1,12 +1,26 @@
+type state = {
+    name: string,
+    value: string
+}
 export default class Store {
-    static store: Store
+    public state: state[] = []
+    static store: Store = new Store()
     private constructor() {
         console.log()
     }
-    public static getInstance() {
-        if (!this.store) {
-            this.store = new Store()
+    public getAllItem() {
+        return this.state
+    }
+    public getItem(key: string) {
+        return this.state.find(item => key === item.name)?.value
+    }
+    public setItem(key: string, value: string) {
+        let isExist = this.state.find(item => key === item.name)
+        if (!isExist) {
+            this.state.push({ name: key, value })
+        } else {
+            let index = this.state.findIndex(item => key === item.name)
+            this.state[index].value = value
         }
-        return this.store
     }
 }

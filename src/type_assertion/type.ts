7637137870c10026122218ee13137d10110040aa
Killian <killian@8bsolutions.cn>
2022-07-15 08:51:36 +0800

    泛型，和一些高级类型

--- src/type_assertion/type.ts
+++ src/type_assertion/type.ts
@@ -1,12 +1,25 @@
-class Per{
-    address="123"
-    do(){
+class Person {
+    address = "123"
+    do() {
 
     }
 }
-class Stu{
+class Student {
 
 }
-let people = new Per()
+let people = new Person()
 // let result = people as Stu; //类型断言
-let result = <Stu>people //类型转换
+let result = <Student>people //类型转换
+
+const asset = {
+    name: "jack",
+    phone: 1234,
+    address: 'earth'
+}
+type obj = {
+    name: string
+    phone: number
+}
+
+console.log(asset.name)
+export { }
